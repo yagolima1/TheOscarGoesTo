59a568a657f59c3e1aa7d4dc13147f74a8c9dd55
Yago Gonçalves <138723627+yagolima1@users.noreply.github.com>
2024-10-25 19:22:04 -0300

    Add files via upload

---
+++ Scripts SQL/Busca.sql
@@ -0,0 +1,28 @@
+USE oscar_database;
+
+-- SELECT COUNT(*) FROM indicados WHERE "Name" Like "%Natalie Portman%";
+
+-- SELECT * FROM indicados_ao_oscar WHERE nome_do_indicado like "%Natalie Portman%" AND vencedor = "true";
+
+-- SELECT * FROM indicados_ao_oscar WHERE nome_do_indicado like "%Amy Adams%";
+
+-- SELECT * FROM indicados_ao_oscar WHERE nome_do_filme like "%Toy Story%" AND vencedor = "true";
+
+-- SELECT * FROM indicados_ao_oscar WHERE categoria = "ACTRESS" ORDER BY ano_cerimonia DESC;
+
+-- SELECT * FROM indicados_ao_oscar WHERE vencedor = "true" AND categoria = "ACTRESS";
+
+-- UPDATE indicados_ao_oscar SET vencedor = "1" WHERE vencedor = "true";
+-- UPDATE indicados_ao_oscar SET vencedor = "0" WHERE vencedor = "false";
+
+-- SELECT nome_do_filme, cerimonia FROM indicados_ao_oscar WHERE nome_do_filme like "%Crash";
+
+-- SELECT * FROM indicados_ao_oscar WHERE nome_do_filme like "Central%";
+
+-- SELECT * FROM indicados_ao_oscar WHERE nome_do_indicado like "%Denzel Washington%" AND vencedor = "1";
+
+-- SELECT * FROM indicados_ao_oscar WHERE categoria = "BEST PICTURE" AND vencedor = "1";
+
+-- SELECT * FROM indicados_ao_oscar WHERE vencedor = "1" AND categoria = "BEST PICTURE" AND categoria = "DIRECTING";
+
+-- SELECT nome_do_indicado, ano_cerimonia FROM indicados_ao_oscar WHERE nome_do_indicado like "%Denzel Washington%" AND nome_do_indicado like "%Jamie Foxx%";
